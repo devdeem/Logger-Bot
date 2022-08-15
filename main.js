1689d00b5111d9155472fee6e1e948fc75340b68
DEEM <91419390+DEEM-0001@users.noreply.github.com>
2022-08-15 15:39:54 +0200

    soon

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-xd
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const config = require('./config.json');
+const colors = require('colors');
+
+client.on('ready', () => {
+    console.log(`${`[DISCORD]`.brightBlue} ${`Logged in as`.white} ${`${client.user.tag}`.brightGreen}`)
+});
+
+client.login(process.env.TOKEN)
